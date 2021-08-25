dd4905e110e5cdbe2c2fdba41d4172ed7e4517e4
karuniasentosa <82386912+karuniasentosa@users.noreply.github.com>
2021-08-25 17:12:32 +0700

    Update login-script.js
    Perbaiki login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -6,11 +6,13 @@
 */
 const loginFormElement = document.querySelector('#loginForm');
 
+
 /** 
 * Membuat variabel inputEmailElement untuk tampilan input email.
 * @constant {HTMLElement}
 */
 const inputEmailElement = document.querySelector('#inputEmail');
+
 
 /** 
 * Membuat variabel inputPasswordElement untuk tampilan input password.
@@ -18,11 +20,13 @@ const inputEmailElement = document.querySelector('#inputEmail');
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
 /** 
  * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
  * @constant {string}
  */
 const expectedEmail = 'admin@dicoding.com';
+
 
 /** 
  * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
@@ -30,21 +34,25 @@ const expectedEmail = 'admin@dicoding.com';
  */
 const expectedPassword = 'superpassword';
 
+
 /* Comment: Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  
   /** 
    * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
    * @constant {string}
    */
   const email = inputEmailElement.value;
   
+  
   /** 
    * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
    * @constant {string}
    */
   const password = inputPasswordElement.value;
+  
   
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
@@ -55,5 +63,6 @@ loginFormElement.addEventListener('submit', function(event) {
     
      /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
+    
   }
 });
